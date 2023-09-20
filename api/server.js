1ede9ad13f5d019757b9415bc6bc4870e438781b
kelvin <117889948+kelvin-ndoma@users.noreply.github.com>
2023-09-20 12:41:25 +0300

    Update server.js

--- api/server.js
+++ api/server.js
@@ -5,22 +5,11 @@ const router = jsonServer.router('db.json')
 const middlewares = jsonServer.defaults()
 
 server.use(middlewares)
-server.use(jsonServer.bodyParser)
-
-// Create a new subscriber
-server.post('/subscribers', (req, res) => {
-  const { name, email } = req.body;
-
-  if (!name || !email) {
-    res.status(400).json({ error: "Name and email are required." });
-  } else {
-    const newSubscriber = { name, email };
-    const db = router.db;
-    db.get("subscribers").push(newSubscriber).write();
-    res.status(201).json(newSubscriber);
-  }
-});
-
+// Add this before server.use(router)
+server.use(jsonServer.rewriter({
+    '/api/*': '/$1',
+    '/blog/:resource/:id/show': '/:resource/:id'
+}))
 server.use(router)
 server.listen(3000, () => {
     console.log('JSON Server is running')
